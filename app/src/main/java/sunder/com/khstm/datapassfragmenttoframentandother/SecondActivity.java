49c27d75b6f44755027b458951787b8f24a5b5cf
Sunder <sunderrrrrrrr345@users.noreply.github.com>
2017-10-18 16:11:49 +0530

    Add files via upload

--- app/src/main/java/sunder/com/khstm/datapassfragmenttoframentandother/SecondActivity.java
+++ app/src/main/java/sunder/com/khstm/datapassfragmenttoframentandother/SecondActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
-String message;
+String message,message1="2";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,7 +21,20 @@ String message;
         Toast.makeText(this, "Hello:"+message, Toast.LENGTH_SHORT).show();
 
 
-        //read how to send data activity to fragment on javatpoint
+        //read how to send data same activity to fragment on javatpoint
+
+
+
+
+
 
     }
+
+    //another way fetch data activity to fragment
+
+    public String getMyData() {
+        return message1;
+    }
+
+    //some way are interface and sharedPrefrence
 }
