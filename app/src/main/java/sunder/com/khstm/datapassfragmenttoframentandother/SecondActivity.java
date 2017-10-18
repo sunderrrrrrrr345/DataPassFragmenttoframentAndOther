d68af6535ea2097292167dff00be7e3acd12d03e
Sunder <sunderrrrrrrr345@users.noreply.github.com>
2017-10-18 16:03:06 +0530

    Add files via upload

--- app/src/main/java/sunder/com/khstm/datapassfragmenttoframentandother/SecondActivity.java
+++ app/src/main/java/sunder/com/khstm/datapassfragmenttoframentandother/SecondActivity.java
@@ -1,13 +1,27 @@
 package sunder.com.khstm.datapassfragmenttoframentandother;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
-
+String message;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+
+
+
+        //fetch data from ReceptionHomeFragment
+        Intent intent = getIntent();
+        message = intent.getStringExtra("Regular_visitor");
+        Toast.makeText(this, "Hello:"+message, Toast.LENGTH_SHORT).show();
+
+
+        //read how to send data activity to fragment on javatpoint
+
     }
 }
